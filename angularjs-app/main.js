ce1cc32cc6acdb33f5c33e92c8f4d8c0186de14a
Yassire El Hani <yassire@evident.studio>
2023-12-12 15:35:27 +0000

    convert legacy to single spa

--- angularjs-app/main.js
+++ angularjs-app/main.js
@@ -1,3 +1,18 @@
+System.register([], function (_export) {
+  return {
+    execute: function () {
+      _export(
+        window.singleSpaAngularjs.default({
+          angular: angular,
+          mainAngularModule: "main-module",
+          uiRouter: true,
+          preserveGlobal: false,
+        })
+      );
+    },
+  };
+});
+
 var app = angular.module("main-module", ["ui.router"]);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
